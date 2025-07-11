2ef397c7c43167ccb988df1c291a3a231cad7cd8
Mark <markkranenburg@hotmail.nl>
2025-07-11 15:24:34 +0200

    move form logic to main.js

--- js/main.js
+++ js/main.js
@@ -151,7 +151,54 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         mouseDownOutside = false;
     }
-    
+
+    // Sol Form
+    const sollicitatieForm = document.getElementById("sollicitatieForm");
+    if (sollicitatieForm) {
+        sollicitatieForm.addEventListener("submit", function (e) {
+            e.preventDefault();
+
+            var formData = new FormData(this);
+
+            fetch("./php/sendsolemail.php", {
+                method: "POST",
+                body: formData
+            })
+                .then(response => response.text())
+                .then(data => {
+                    alert(data);
+                    sollicitatieForm.reset();
+                })
+                .catch(error => {
+                    alert("Oops — something went wrong.");
+                    console.error(error);
+                });
+        });
+    }
+
+    const opdrachtgeverForm = document.getElementById("opdrachtgeverForm");
+    if (opdrachtgeverForm) {
+        opdrachtgeverForm.addEventListener("submit", function (e) {
+            e.preventDefault();
+
+            var formData = new FormData(this);
+
+            fetch("./php/sendopdemail.php", {
+                method: "POST",
+                body: formData
+            })
+                .then(response => response.text())
+                .then(data => {
+                    alert(data);
+                    opdrachtgeverForm.reset();
+                })
+                .catch(error => {
+                    alert("Oops — something went wrong.");
+                    console.error(error);
+                });
+        });
+    }
+
     // URL management functions
     //     function changeUrl(path) {
     //         history.pushState(null, '', path);
