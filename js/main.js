a3153ab24c680c63e10b444b1d99e2e0540e5d58
SachaRethans <s.rethans@qquest.nl>
2025-10-08 18:14:59 +0200

    lenis scroll bibliotheek

--- js/main.js
+++ js/main.js
@@ -26,6 +26,16 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("callback - particles.js config loaded");
   });
 
+  // Initialize Lenis
+  const lenis = new Lenis({
+    autoRaf: true,
+  });
+
+  // Listen for the scroll event and log the event data
+  lenis.on("scroll", (e) => {
+    console.log(e);
+  });
+
   // Slides sollicitatie page in from left (mobile)
   document
     .getElementById("openSolliMobile")
