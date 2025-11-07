c064f2b62e1d5b20f4023536805e8c96f561504d
Mark <markkranenburg@hotmail.nl>
2025-11-07 17:46:17 +0100

    add pintrest layout to blogpage

---
+++ src/articles/article6.js
@@ -0,0 +1,19 @@
+export const article6Content = {
+    id: 6,
+    title: "Wat is een traineeship?",
+    image: "./../images/blog/article6.jpg",
+    date: "2025-01-27",
+    text: `
+    In de zoektocht naar een nieuwe baan zie je ze vaak langskomen: traineeships. Dit kan in alle vormen en maten; je hebt Rijkstraineeships bij de overheid, interne traineeships bij allerlei bedrijven van verzekeraars tot banken, en een groot aanbod aan IT-Traineeships. Maar wat houd een traineeship eigenlijk in, wat is de waarde van een traineeship in de huidige arbeidsmarkt, wat zijn de voordelen voor jou als werkzoekende en hoe sluit Greenlights hierop aan? In dit artikel wordt daar meer over verteld.
+
+    Allereerst, wat is nu eigenlijk een traineeship? Het FNV, de grootste vakbond van Nederland, legt een traineeship uit als volgt: “Een traineeship is een baan voor starters of omscholers waarbij je in een bedrijf wordt opgeleid voor een bepaalde positie. Je krijgt dus interne scholing maar werkt ook met veel verantwoordelijkheden in een startersfunctie. Je krijgt een salaris dat bij die functie hoort.” Meestal duurt een traineeship tussen de één en de drie jaar, afhankelijk wat voor traineeship je volgt. Hierna krijg je in de regel een vast contract bij het bedrijf waar je in opdracht hebt gezeten.
+
+    In de huidige arbeidsmarkt worden er veel traineeships aangeboden in een grote variëteit van bedrijven en instanties, en daar zijn goede redenen voor. Zo is een traineeship een goed middel om een carrière switch te doen. Als je bijvoorbeeld les hebt gegeven maar nu iets in de IT wil doen, is een IT-traineeship een uitstekend instappunt. Je hebt geen opleiding nodig in het vakgebied, zolang je maar goed gemotiveerd bent en verantwoordelijk neemt en hebt.
+    
+    In de zoektocht naar een nieuwe baan zie je ze vaak langskomen: traineeships. Dit kan in alle vormen en maten; je hebt Rijkstraineeships bij de overheid, interne traineeships bij allerlei bedrijven van verzekeraars tot banken, en een groot aanbod aan IT-Traineeships. Maar wat houd een traineeship eigenlijk in, wat is de waarde van een traineeship in de huidige arbeidsmarkt, wat zijn de voordelen voor jou als werkzoekende en hoe sluit Greenlights hierop aan? In dit artikel wordt daar meer over verteld.
+
+    Allereerst, wat is nu eigenlijk een traineeship? Het FNV, de grootste vakbond van Nederland, legt een traineeship uit als volgt: “Een traineeship is een baan voor starters of omscholers waarbij je in een bedrijf wordt opgeleid voor een bepaalde positie. Je krijgt dus interne scholing maar werkt ook met veel verantwoordelijkheden in een startersfunctie. Je krijgt een salaris dat bij die functie hoort.” Meestal duurt een traineeship tussen de één en de drie jaar, afhankelijk wat voor traineeship je volgt. Hierna krijg je in de regel een vast contract bij het bedrijf waar je in opdracht hebt gezeten.
+
+    In de huidige arbeidsmarkt worden er veel traineeships aangeboden in een grote variëteit van bedrijven en instanties, en daar zijn goede redenen voor. Zo is een traineeship een goed middel om een carrière switch te doen. Als je bijvoorbeeld les hebt gegeven maar nu iets in de IT wil doen, is een IT-traineeship een uitstekend instappunt. Je hebt geen opleiding nodig in het vakgebied, zolang je maar goed gemotiveerd bent en verantwoordelijk neemt en hebt.
+    `
+};
