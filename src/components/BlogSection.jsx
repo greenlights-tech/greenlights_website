b90318f8f59837d661a641c2422206bad307d019
Mark <markkranenburg@hotmail.nl>
2026-01-09 11:53:49 +0100

    remove dummy blog component

--- src/components/BlogSection.jsx
+++ src/components/BlogSection.jsx
@@ -1,11 +1,8 @@
-import missieVisieSvg from "../assets/missievisie.svg";
 
 export const BlogSection = ({ className }) => {
   return (
     <section id="blog" className={`blog-component ${className}`}>
-      <section className="info-container-missievisie">
-        <img className="info-missievisie-svg" src={missieVisieSvg} />
-      </section>
+      <h1>TEST!!!!!!!111</h1>
     </section>
   );
 };
