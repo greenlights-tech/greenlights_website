c12d146ce651c49cf48d26e40f9627f7c5cffa9c
Mark <markkranenburg@hotmail.nl>
2026-01-23 17:39:24 +0100

    render markdown correctly

--- src/components/BlogSection.jsx
+++ src/components/BlogSection.jsx
@@ -4,29 +4,54 @@ import matter from 'gray-matter';
 import { Buffer } from 'buffer';
 window.Buffer = Buffer;
 import '../styles/blog.css';
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import rehypeSanitize from "rehype-sanitize";
 
 
 // returnt raw data en image urls met contenct van "blog-content/articles/article*.md bestanden zonder dat het uitmaakt hoeveel article*.md er zijn
 const rawArticleData = Object.values(import.meta.glob('../../blog-content/articles/*.md', { eager: true, query: '?raw', import: 'default' }));
 const imageUrls = Object.values(import.meta.glob('../../blog-content/images/*.{png,jpg,jpeg,svg}', { eager: true, query: '?url', import: 'default' }));
+const backgrounds = ['article-color-darkgreen', 'article-color-yellow', 'article-color-blue', 'article-color-gray', 'article-color-green'];
 
 const articles = rawArticleData.map((articleArray, index) => {
   const { data, content } = matter(articleArray);
   const image = imageUrls[index];
-  return { id: `article-${index}`, ...data, image, content };
+  const background = backgrounds[index % backgrounds.length];
+  return { id: `article-${index}`, ...data, image, content, background };
 });
 
-const ArticleCard = ({ title, content, date, author, excerpt, image, onClick, isExpanded }) => {
+const ArticleCard = ({ title, content, date, author, excerpt, image, onClick, isExpanded, background }) => {
   return (
-    <article className={`article-card ${isExpanded ? 'card-expanded' : ''}`} onClick={onClick}>
+    <article className={`article-card ${isExpanded ? 'card-expanded' : ''} ${background}`} onClick={onClick}>
       <img src={image} alt={title} />
       <h3>{title}</h3>
-      <p>{excerpt}</p>
-      {/* <div className='article-footer'>
-        <p>{author}</p>
-        <small>{date.toLocaleDateString("nl-NL", { day: "2-digit", month: "long", year: "numeric", })}</small>
-      </div> */}
-      {isExpanded && <p>{content}</p>}
+      <p className={`${isExpanded ? 'excerpt' : ''}`} >{excerpt}</p>
+      {isExpanded ? (
+        <>
+          <ReactMarkdown
+            remarkPlugins={[remarkGfm]}
+            rehypePlugins={[rehypeSanitize]}
+            components={{
+              h2: ({ children }) => <h4>{children}</h4>,
+              h3: ({ children }) => <h5>{children}</h5>,
+              ul: ({ children }) => <ul className='ul'>{children}</ul>,
+              li: ({ children }) => <li className='ul'>{children}</li>,
+            }}
+          >
+            {content}
+          </ReactMarkdown>
+          <div className='article-footer'>
+            <small>{author}</small>
+            <small>{date.toLocaleDateString("nl-NL", { day: "2-digit", month: "long", year: "numeric", })}</small>
+          </div>
+        </>
+      ) : (
+        <div className='article-footer'>
+          <small>{author}</small>
+          <small>{date.toLocaleDateString("nl-NL", { day: "2-digit", month: "long", year: "numeric", })}</small>
+        </div>
+      )}
     </article>
   );
 };
