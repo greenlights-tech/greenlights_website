a93a164fa8770ff4a430843e82f68a4bd6303609
Sacha Rethans <s.rethans@qquest.nl>
2026-06-05 14:23:32 +0200

    homepage met cards aangepast

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -5,6 +5,11 @@ import { useRef } from "react";
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import cardImage1 from "../assets/cards-1.jpg";
+import cardImage2 from "../assets/cards-2.jpg";
+import cardImage3 from "../assets/cards-3.jpg";
+import cardImage4 from "../assets/cards-4.jpg";
+import cardImage5 from "../assets/cards-5.jpg";
 
 gsap.registerPlugin(useGSAP, ScrollTrigger);
 // import { CustomEase } from "gsap/CustomEase";
@@ -56,23 +61,28 @@ export const AboutSection = ({ className }) => {
   const data = [
     {
       title: "Selectie op Motivatie & Fit",
-      text: "Onze selectie focust op de menselijke factor...",
+      text: "Onze selectie focust op de menselijke factor: we screenen kandidaten primair op motivatie, leervermogen en teamfit, waardoor we sneller een duurzame match garanderen.",
+      image: cardImage1,
     },
     {
       title: "Praktijkgerichte Opleiding",
-      text: "Uw behoeften staan centraal...",
+      text: "Uw behoeften staan centraal. De kandidaten doorlopen een op maat gemaakte, praktijkgerichte training in de exacte IT-skills die zij direct in hun toekomstige rol nodig hebben.",
+      image: cardImage2,
     },
     {
       title: "Persoonlijke Begeleiding",
-      text: "Onze coaches bieden holistische begeleiding...",
+      text: "Onze coaches bieden holistische begeleiding, zowel op technische vaardigheden als op cruciale soft skills (communicatie, probleemoplossing). Dit zorgt voor professionele én persoonlijke groei.",
+      image: cardImage3,
     },
     {
       title: "Directe Inzet op Maat",
-      text: "Dankzij de gerichte selectie...",
+      text: "Dankzij de gerichte selectie en training zijn de starters sneller inzetbaar. Ze dragen direct bij aan uw projecten en werkzaamheden, zonder lange inwerktrajecten.",
+      image: cardImage4,
     },
     {
       title: "Duurzame Detavast-constructie",
-      text: "Bouwen aan slagkracht...",
+      text: " Bouwen aan slagkracht, geen flexibele schil. De detavast-formule geeft zowel u als de IT-starter de zekerheid om na een proefperiode over te gaan naar een vast dienstverband, wat leidt tot continuïteit en kennisbehoud.",
+      image: cardImage5,
     },
   ];
 
@@ -89,9 +99,17 @@ export const AboutSection = ({ className }) => {
             className={`card card-${i}`}
             ref={(el) => (cardsRef.current[i] = el)}
           >
+
+
             <div className="card-content">
-              <h3>{card.title}</h3>
-              <p>{card.text}</p>
+              <img className="card-image" src={card.image} alt={card.title} />
+
+
+              <div className="card-text-wrapper">
+                <h3 className="card-title">{card.title}</h3>
+                <p className="card-text">{card.text}</p>
+              </div>
+
             </div>
           </div>
         ))}
