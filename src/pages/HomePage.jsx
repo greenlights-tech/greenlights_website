5bcda327093e28887ab0c232e568cc1b39838e2c
Sacha Rethans <s.rethans@qquest.nl>
2026-06-10 09:28:47 +0200

    test

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -138,13 +138,13 @@ export const HomePage = () => {
         tl.current = gsap.timeline({
           paused: true,
 
-          onStart: () => { ScrollSmoother.get()?.paused(true); document.body.style.overflow = "hidden"; },
+          onStart: () => { ScrollSmoother.get()?.paused(true); document.body.style.overflowY = "hidden"; },
           onComplete: () => {
             introHasRunGlobal = true;
             setIntroFinished(true);
 
             ScrollSmoother.get()?.paused(false);
-            document.body.style.overflow = "";
+            document.body.style.overflowY = "";
 
             requestAnimationFrame(() => {
               ScrollTrigger.refresh(true);
