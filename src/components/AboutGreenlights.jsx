808838a000fe2c6f224607ea33bad50f89b2a866
Sacha Rethans <s.rethans@qquest.nl>
2026-06-15 17:05:53 +0200

    info greenlights component toegevoegd

--- src/components/AboutGreenlights.jsx
+++ src/components/AboutGreenlights.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import heroImage1 from "../assets/cards-1.jpg";
 
 gsap.registerPlugin(useGSAP, ScrollTrigger);
 
@@ -17,21 +18,22 @@ export const AboutGreenlights = ({ className }) => {
   return (
     <section
       id="over-ons"
-      className={`about-component ${className}`}
+      className={`about-greenlights-component ${className}`}
     //   ref={container}
     >
-      <section className="info-container-effect">
-        <div className="pin-height">
-          <div className="info-container">
-            <div className="info-title-wie">Over ons</div>
-            <div className="alineas">
-              <div className="first alinea-mobile"></div>
-              <div className="second alinea alinea-mobile">
+<div className="info-title-container"></div>
+         <h2 className="info-title">Over ons</h2>
+      <section className="info-container">
+            <div className="content-container">
+                <div className="first alinea">
                 Greenlights is hét IT-talentontwikkelingsbureau voor
                 organisaties die duurzaam willen investeren in hun team. Wij
                 geloven niet in stapels cv's. Onze aanpak is persoonlijk en
                 doelgericht: we selecteren gemotiveerde IT-starters op basis van
-                motivatie, leervermogen en culturele fit. Deze toptalenten
+                motivatie, leervermogen en culturele fit. 
+              </div>
+              <div className="second alinea">
+                Deze toptalenten
                 worden praktijkgericht opgeleid in de benodigde IT-skills en
                 intensief begeleid door ervaren coaches. Door onze
                 detavast-constructie groeit de starter uit tot een waardevol en
@@ -39,10 +41,11 @@ export const AboutGreenlights = ({ className }) => {
                 schil. Zo maken we het aantrekken van passend IT-talent
                 eenvoudiger én effectiever.
               </div>
-              <div className="third alinea alinea-mobile"></div>
             </div>
-          </div>
-        </div>
+            <div className="info-image-container">
+               <img className="card-image" src={heroImage1} alt="Over Greenlights" />
+            </div>
+              
       </section>
     </section>
   );
